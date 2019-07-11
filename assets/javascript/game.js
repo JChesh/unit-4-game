2d2ff3a2966b99aa7dab0e95c1bc3c78ac8fbc03
JChesh <jonathans.cheshire@gmail.com>
2019-07-11 16:01:56 -0400

    Start of page with variable

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+//Globals
+
+//Wins and losses
+var wins = 0;
+var loss = 0;
+
+//User and Target scores
+var yourScore = 0;
+var targetScore = 0;
+
+//Storing var Crystals as an object
+var crystals = {
+    crystalOne:{
+        name: "one",
+        value: 0
+    },
+
+    crystalTwo:{
+        name: "one",
+        value: 0
+    },
+
+    crystalThree:{
+        name: "one",
+        value: 0
+    },
+
+    crystalFour:{
+        name: "one",
+        value: 0
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
